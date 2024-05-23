df8037175cf0b704ab4899503dccbeac39909c9d
Huy Pham <huypt110402@gmail.com>
2024-05-23 16:02:05 +0700

    update model

--- SE160548_IdetityAjaxASP.NETCoreWebAPI/Models/CreateProductModel.cs
+++ SE160548_IdetityAjaxASP.NETCoreWebAPI/Models/CreateProductModel.cs
@@ -1,12 +1,22 @@
-﻿namespace SE160548_IdetityAjaxASP.NETCoreWebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE160548_IdetityAjaxASP.NETCoreWebAPI.Models
 {
     public class CreateProductModel
     {
+
         public int ProductId { get; set; }
+        [Required]
         public int? CategoryId { get; set; }
+        [Required]
         public string ProductName { get; set; } = null!;
+        [Required]
         public string Weight { get; set; } = null!;
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage = "Unit price must be a positive number.")]
         public decimal UnitPrice { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "Units in stock must be a non-negative number.")]
         public int UnitsInStock { get; set; }
     }
 }
--- SE160548_IdetityAjaxASP.NETCoreWebAPI/Models/LoginModel.cs
+++ SE160548_IdetityAjaxASP.NETCoreWebAPI/Models/LoginModel.cs
@@ -1,8 +1,12 @@
-﻿namespace SE160548_IdetityAjaxASP.NETCoreWebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE160548_IdetityAjaxASP.NETCoreWebAPI.Models
 {
     public class LoginModel
     {
+        [Required]
         public string Email { get; set; } = null!;
+        [Required]
         public string Password { get; set; } = null!;
     }
 }
